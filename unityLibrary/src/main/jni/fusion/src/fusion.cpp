cb86a8376efc7849841090f6b1c0a84823af0ff5
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-03-25 16:26:16 -0400

    fixed on hybrid animals

--- unityLibrary/src/main/jni/fusion/src/fusion.cpp
+++ unityLibrary/src/main/jni/fusion/src/fusion.cpp
@@ -31,7 +31,7 @@ int il2cpp_init_hook(char *domain_name)
         setenv("FUSION_GAME_BINARY", libmain_get_override_il2cpp_path(), 1);
         setenv("FUSION_GAME_DATA_DIR", config.appDataDirectory.c_str(), 1);
         setenv("FUSION_APP_DATA_DIR", config.appDataDirectory.c_str(), 1);
-        setenv("FUSION_UNITY_VERSION", "2022.3.44f1", 1);
+        setenv("FUSION_UNITY_VERSION", "2021.3.43f1", 1);
 
         fs::path bepInExCoreDirectory = fs::path(config.bepInExDirectory) / "core";
 
